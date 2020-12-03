6646a1a9f60a765795aa936cbf4c76301f34420d
Frank666 <frank.esteban.a@gmail.com>
2020-12-03 03:12:35 -0600

    fix css

--- grayboxchallenge/src/components/menu/subMenuTitle.component.js
+++ grayboxchallenge/src/components/menu/subMenuTitle.component.js
@@ -3,8 +3,11 @@ import React from 'react'
 const SubMenuTitle = () => {
     return (
         <div>
-            RESET
-            FILTER
+            <h4> 
+                <div className="left">Reset</div>
+                <div className="right">Filter</div>
+                <div className="clearLeft"/>
+            </h4>
         </div>
     )
 }
